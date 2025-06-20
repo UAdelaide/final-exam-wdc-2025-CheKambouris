2ce61c034dd8b17b1cb33a6aed1c79c13931290c
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:10:22 +0000

    [automatic commit] 2025-06-20 03:10:22 UTC 161.8.212.82

--- part1/database.js
+++ part1/database.js
@@ -46,7 +46,14 @@ VALUES
   ((SELECT user_id FROM Users WHERE username = 'sploot'), 'Tom', 'small'),
   ((SELECT user_id FROM Users WHERE username = 'sploot'), 'Jerry', 'medium'),
   ((SELECT user_id FROM Users WHERE username = 'sploot'), 'Sanders', 'large');`);
-  
+  `INSERT INTO WalkRequests
+	(dog_id, requested_time, duration_minutes, location, status)
+VALUES
+	((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10T08:00:00', 30, 'Parklands', 'open'),
+	((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10T09:30:00', 45, 'Beachside Ave', 'accepted'),
+	((SELECT dog_id FROM Dogs WHERE name = 'Tom'), '2025-06-11T09:30:00', 25, 'Burnside', 'cancelled'),
+	((SELECT dog_id FROM Dogs WHERE name = 'Jerry'), '2025-06-11T012:30:00', 40, 'Burnside', 'open'),
+	((SELECT dog_id FROM Dogs WHERE name = 'Sanders'), '2025-06-11T014:30:00', 120, 'Burnside', 'accepted');`);
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
