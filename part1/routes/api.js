114a1c3e759e1fe72254a7454ca5271d073649f0
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:33:36 +0000

    [automatic commit] 2025-06-20 03:33:36 UTC 161.8.212.82

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const pool = require('../database');
 
-router.get('/dogs', function(req, res, next) {
-  const [rows] = pool.query(
+router.get('/dogs', async function(req, res, next) {
+  const [rows] = await pool.query(
     `SELECT Dogs.dog_name, Dogs.size, User.username AS owner_username
     FROM Dogs
     INNER JOIN Users
