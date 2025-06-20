5029933742ff5b6c0b01600616457af1c1a6d554
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:25:58 +0000

    [automatic commit] 2025-06-20 02:25:58 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -16,6 +16,6 @@
 BEGIN TRANSACTION;
 INSERT INTO Users (username, email, password_hash, role)
 VALUES (alice123, alice@example.com, hashed123, 'owner');
-
+INSERT INTO Dogs ()
 LAST_INSERT_ID()
 COMMIT;
