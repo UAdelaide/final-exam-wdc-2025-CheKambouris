f5de12b87d2c75b3b9ef69818107283572f8aea6
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 07:57:11 +0000

    [automatic commit] 2025-06-20 07:57:11 UTC 161.8.212.82

--- part2/app.js
+++ part2/app.js
@@ -17,7 +17,6 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/dogs', dogRoutes);
