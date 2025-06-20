572d5fc025b7398df7cb7976388f09a4e31f52f4
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:43:07 +0000

    [automatic commit] 2025-06-20 02:43:07 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -1,10 +1,3 @@
-`
-    Five walk requests:
-        A request for Max at 2025-06-10 08:00:00 for 30 minutes at Parklands, with status open.
-        A request for Bella at 2025-06-10 09:30:00 for 45 minutes at Beachside Ave, with status accepted.
-        Three more walk requests with details of your choosing.
-`
-
 INSERT INTO Users
 	(username, email, password_hash, role)
 VALUES
@@ -31,3 +24,9 @@ VALUES
 	(LAST_INSERT_ID(), '2025-06-10T09:30:00', 30, 'Parklands', 'open');
 COMMIT;
 
+`
+    Five walk requests:
+        A request for Max at 2025-06-10 08:00:00 for 30 minutes at Parklands, with status open.
+        A request for Bella at 2025-06-10 09:30:00 for 45 minutes at Beachside Ave, with status accepted.
+        Three more walk requests with details of your choosing.
+`
