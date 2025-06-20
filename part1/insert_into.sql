a3065508cca0691b73bc389102fb535fc9ccd25f
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:31:00 +0000

    [automatic commit] 2025-06-20 02:31:00 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -19,5 +19,5 @@ VALUES (alice123, alice@example.com, hashed123, 'owner');
 INSERT INTO Dogs (owner_id, name, size)
 VALUES (LAST_INSERT_ID(), 'Max', 'medium');
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
-VALUES (LAST_INSERT_ID(), '2025-06-10T09:30:00', 30, )
+VALUES (LAST_INSERT_ID(), '2025-06-10T09:30:00', 30, 'Parklands', 'open')
 COMMIT;
