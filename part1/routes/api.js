a9ed1cee7f30ddefe6965b98732b2d1382dcf12d
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:25:51 +0000

    [automatic commit] 2025-06-20 03:25:51 UTC 161.8.212.82

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -4,7 +4,9 @@ const conn = require('../database');
 
 router.get('/dogs', function(req, res, next) {
   const [rows] = conn.execute('SELECT * FROM Dogs');
+  res.send(rows.map((row) => {
   
+  }))
 });
 
 router.get('/walkrequests/open', function(req, res, next) {
