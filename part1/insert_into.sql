ee5a8972a8bb727e422837c37849e79ba86f33c6
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:51:12 +0000

    [automatic commit] 2025-06-20 02:51:12 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -1,11 +1,11 @@
 INSERT INTO Users
 	(username, email, password_hash, role)
 VALUES
-	(alice123, alice@example.com, hashed123, 'owner'),
-	(bobwalker, bob@example.com, hashed456, 'walker'),
-	(carol123, carol@example.com, hashed789, 'owner'),
-	(sploot, spoot@example.com, supersecure, 'walker'),
-	(edgar, edgar@allen.poe, raven333, 'walker');
+	('alice123', 'alice@example.com', 'hashed123', 'owner'),
+	('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+	('carol123', 'carol@example.com', 'hashed789', 'owner'),
+	('sploot', 'spoot@example.com', 'supersecure', 'walker'),
+	('edgar', 'edgar@allen.poe', 'raven333', 'walker');
 INSERT INTO
 	Dogs (owner_id, name, size)
 VALUES
