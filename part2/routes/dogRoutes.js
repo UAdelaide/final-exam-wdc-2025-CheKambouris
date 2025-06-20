e4990302bf5b7f609bdbf904188aaf09cb51c851
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 07:53:10 +0000

    [automatic commit] 2025-06-20 07:53:10 UTC 161.8.212.82

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -5,7 +5,21 @@ const db = require('../models/db');
 // GET user dogs
 router.get('/', async (req, res) => {
   if (req.session.user) {
-    db.execute('');
+    try {
+    const [rows] = await db.execute(
+      `SELECT
+        Dogs.name AS dog_name,
+        Dogs.size,
+        Users.username AS owner_username
+      FROM Dogs
+      INNER JOIN Users
+      ON Dogs.owner_id = Users.user_id;`
+    );
+    res.json(rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
   } else {
     res.send(401);
   }
