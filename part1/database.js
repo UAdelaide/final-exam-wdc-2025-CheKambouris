76ac9c4e7559698c790252067ef128c868fc9a73
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:02:58 +0000

    [automatic commit] 2025-06-20 03:02:58 UTC 161.8.212.82

--- part1/database.js
+++ part1/database.js
@@ -27,7 +27,7 @@ let db;
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
-      await db.execute(fs.readFile('insert_into.sql'));
+      await db.execute(fs.readFileSync('insert_into.sql', utf));
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
