ffe7295c9bae8ae6bdf28b4446f70f1b2998c727
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:44:47 +0000

    [automatic commit] 2025-06-20 02:44:47 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -17,7 +17,7 @@ VALUES
 INSERT INTO WalkRequests
 	(dog_id, requested_time, duration_minutes, location, status)
 VALUES
-	((SELECT dog_id FROM Dogs WHERE name = 'Max', '2025-06-10T09:30:00'), 30, 'Parklands', 'open'),
+	((SELECT dog_id FROM Dogs WHERE name = 'Max', '2025-06-10T08:00:00'), 30, 'Parklands', 'open'),
 	((SELECT dog_id FROM Dogs WHERE name = 'alice123', '2025-06-10T09:30:00'), 30, 'Parklands', 'open'),
 	((SELECT dog_id FROM Dogs WHERE name = 'alice123', '2025-06-10T09:30:00'), 30, 'Parklands', 'open'),
 	((SELECT dog_id FROM Dogs WHERE name = 'alice123', '2025-06-10T09:30:00'), 30, 'Parklands', 'open'),
