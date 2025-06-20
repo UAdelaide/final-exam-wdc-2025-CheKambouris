db3298765dd987dd52c924a5e36ae9c6cff752e0
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 07:52:08 +0000

    [automatic commit] 2025-06-20 07:52:08 UTC 161.8.212.82

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-// GET all walk requests (for walkers to view)
+// GET user dogs
 router.get('/', async (req, res) => {
+  if (req.session.user) {
+
+  }
   
 });
