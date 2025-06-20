afb32d2d9bb4f9ca74ade6d9875ca0e0f428e31c
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:29:39 +0000

    [automatic commit] 2025-06-20 02:29:39 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -18,5 +18,6 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES (alice123, alice@example.com, hashed123, 'owner');
 INSERT INTO Dogs (owner_id, name, size)
 VALUES (LAST_INSERT_ID(), 'Max', 'medium');
-INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status, created_at)
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+VALUES ()
 COMMIT;
