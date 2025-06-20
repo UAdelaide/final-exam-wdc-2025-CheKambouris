31ec1eb0a2fb5aa30ab269059e8630407cd32c1c
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:17:07 +0000

    [automatic commit] 2025-06-20 03:17:07 UTC 161.8.212.82

--- part1/app.js
+++ part1/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/api');
 var db = require('./database');
 
 var app = express();
