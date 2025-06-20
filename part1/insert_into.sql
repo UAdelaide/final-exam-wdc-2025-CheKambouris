991ad89d0a0a8c150faefdd85d123bcd89519375
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:40:46 +0000

    [automatic commit] 2025-06-20 02:40:46 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -17,16 +17,15 @@
 INSERT INTO Users
 	(username, email, password_hash, role)
 VALUES
-	(alice123, alice@example.com, hashed123, 'owner')
-	(bobwalker, bob@example.com, hashed456, 'walker')
-	(carol123, carol@example.com, hashed789, 'owner')
-	(sploot, runner@example.com, supersecure, 'walker')
-	(edgar, edgar@allen.poe, raven333hound, 'owner')
-;
+	(alice123, alice@example.com, hashed123, 'owner'),
+	(bobwalker, bob@example.com, hashed456, 'walker'),
+	(carol123, carol@example.com, hashed789, 'owner'),
+	(sploot, runner@example.com, supersecure, 'walker'),
+	(edgar, edgar@allen.poe, raven333hound, 'owner');
 INSERT INTO
 	Dogs (owner_id, name, size)
 VALUES
-	((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium')
+	((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ;
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES (LAST_INSERT_ID(), '2025-06-10T09:30:00', 30, 'Parklands', 'open')
