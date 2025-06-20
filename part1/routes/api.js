6ed615332823e53c5b4ddc06357953fbdf09d746
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:58:30 +0000

    [automatic commit] 2025-06-20 03:58:30 UTC 161.8.212.82

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -67,8 +67,6 @@ router.get('/walkers/summary', async function(req, res, next) {
     FROM Users
     INNER JOIN WalkRatings
       ON WalkRatings.walker_id = Users.user_id
-    INNER JOIN WalkRequests
-      ON WalkRequests.
     WHERE Users.role = 'walker';` // Implicit if there was a constraint
   );
   res.send(rows);
