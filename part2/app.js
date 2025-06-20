164561a6db1f0c89ac82d9257fb633a854a7b66e
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 08:13:42 +0000

    [automatic commit] 2025-06-20 08:13:42 UTC 161.8.212.82

--- part2/app.js
+++ part2/app.js
@@ -20,6 +20,25 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/dogs', (req, res, next) => {
+  router.get('/dogs', async function(req, res, next) {
+  try {
+    const [rows] = await pool.query(
+      `SELECT
+        Dogs.name AS dog_name,
+        Dogs.size,
+        Users.username AS owner_username
+      FROM Dogs
+      INNER JOIN Users
+      ON Dogs.owner_id = Users.user_id;`
+    );
+    res.json(rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+})
 
 // Export the app instead of listening here
 module.exports = app;
