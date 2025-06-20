737cbaca89237435ce1b8886fc6fcc9724ceb7f5
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 03:05:19 +0000

    [automatic commit] 2025-06-20 03:05:19 UTC 161.8.212.82

--- part1/database.js
+++ part1/database.js
@@ -12,9 +12,7 @@ let db;
       password: '' // Set your MySQL root password
     });
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
-    await connection.end();
+    // Assume that the database has been setup
 
     // Now connect to the created database
     db = await mysql.createConnection({
