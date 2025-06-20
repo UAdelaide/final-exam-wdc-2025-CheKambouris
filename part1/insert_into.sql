86e5008675b329cbacb095b5ec71ba670ced47df
Che Kambouris <chekambouris@protonmail.com>
2025-06-20 02:34:01 +0000

    [automatic commit] 2025-06-20 02:34:01 UTC 161.8.212.82

--- part1/insert_into.sql
+++ part1/insert_into.sql
@@ -19,7 +19,7 @@ INSERT INTO
 	Users (username, email, password_hash, role)
 VALUES
 	(alice123, alice@example.com, hashed123, 'owner')
-	(bobwalker, alice@example.com, hashed123, 'owner')
+	(bobwalker, bob@example.com, hashed456, 'walker')
 	(alice123, alice@example.com, hashed123, 'owner')
 	(alice123, alice@example.com, hashed123, 'owner')
 	(alice123, alice@example.com, hashed123, 'owner')
